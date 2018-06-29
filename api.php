0c06ba54f5153e6a5d8c2b1dc2dd2a32b32548ff
raymon <1536252162@qq.com>
2018-06-29 10:44:31 +0800

    111

---
+++ api.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: raymon
+ * Date: 2018/6/29
+ * Time: 上午10:41
+ */
+try {
+    print_r($_REQUEST);die;
+//    $payload = $this->getParams('payload');
+//    $requestParams = json_decode($payload, true);
+//    if ($requestParams['ref'] == 'refs/heads/develop') { //develop 分支自动发布到beta
+//        $payload = 'need-pub: ' . $payload;
+//        $shellPath = BASE_DIR . '/publish/';
+//        $shellScript = 'develop_publish.sh';
+//        $publishLog = $shellPath . 'log/publish.log';
+//        exec('cd ' . $shellPath . ' && chmod a+x ' . $shellScript . ' && ./' . $shellScript . ' >' . $publishLog);
+//    }
+//    Bw_File_Log::getInstance('webhook')->write(date('Y-m-d H:i:s') . ' -- ' . $payload);
+//    $this->sendJson(true, ['message' => '回调成功!']);
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
